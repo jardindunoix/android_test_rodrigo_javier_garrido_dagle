98f25b0c1cc939caf82fc749664ebb7bbd67513e
rj <rj@gigioberto.com>
2024-10-08 18:59:58 -0300

    dto classes

---
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/data/network/dto/StoryUrlDto.kt
@@ -0,0 +1,12 @@
+package cl.rodrigo_javier_garrido_dagle.mobile_test.data.network.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class StoryUrlDto(
+   @SerializedName("matchLevel")
+   val matchLevel: String,
+   @SerializedName("matchedWords")
+   val matchedWords: List<String>,
+   @SerializedName("value")
+   val value: String
+)
