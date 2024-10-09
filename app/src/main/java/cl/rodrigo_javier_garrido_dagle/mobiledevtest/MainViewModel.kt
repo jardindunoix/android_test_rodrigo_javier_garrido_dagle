273ac6f1b6562455a9b4497994493a5a4c84882c
rj <rj@gigioberto.com>
2024-10-09 13:16:59 -0300

    use case to getting main list

--- app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/MainViewModel.kt
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/MainViewModel.kt
@@ -1,45 +1,43 @@
 package cl.rodrigo_javier_garrido_dagle.mobiledevtest
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.repositories.ConnectivityRepository
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.usecases.NetworkUsecases
-import cl.rodrigo_javier_garrido_dagle.mobiledevtest.utilities.Constants.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-   connectivityRepository: ConnectivityRepository,
-   private val networkUsecases: NetworkUsecases,
+    connectivityRepository: ConnectivityRepository,
+    private val networkUsecases: NetworkUsecases,
 ) : ViewModel() {
-   val isOnline = connectivityRepository.isConnected.asLiveData()
+    val isOnline = connectivityRepository.isConnected.asLiveData()
 
-   init {
-      getAllHits()
-   }
+    init {
+        getAllHits()
+    }
 
-   private fun getAllHits() {
-      viewModelScope.launch {
-         val list = networkUsecases.invoke()
+    private fun getAllHits() {
+        viewModelScope.launch {
+            networkUsecases.invoke()
 
-         list!!.map { hit ->
-            val author = hit.author
-            val createdAtId = hit.createdAtI
-            val title = hit.title ?: hit.storyTitle
-            val storyLink = hit.storyUrl ?: "no link"
-            val storyText = hit.storyText ?: hit.commentText
-            val storyDate = hit.updatedAt ?: hit.createdAt
+//            list!!.map { hit ->
+//                val author = hit.author
+//                val createdAtId = hit.createdAtI
+//                val title = hit.title ?: hit.storyTitle
+//                val storyLink = hit.storyUrl ?: "no link"
+//                val storyText = hit.storyText ?: hit.commentText
+//                val storyDate = hit.updatedAt ?: hit.createdAt
+//
+//                Log.d(
+//                    TAG,
+//                    "$createdAtId  -- $storyText"
+//                )
 
-            Log.d(
-               TAG,
-               "$createdAtId  -- $storyText"
-            )
-
-         }
-      }
-   }
+        }
+    }
+}
 }
--- app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/domain/usecases/NetworkUsecases.kt
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/domain/usecases/NetworkUsecases.kt
@@ -1,17 +1,15 @@
 package cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.usecases
 
-import android.util.Log
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.mappers.networkToDomain
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.repositories.NetworkRepositoryImpl
-import cl.rodrigo_javier_garrido_dagle.mobiledevtest.utilities.Constants.TAG
 import javax.inject.Inject
 
 class NetworkUsecases @Inject constructor(
-   private val networkRepository: NetworkRepositoryImpl,
+    private val networkRepository: NetworkRepositoryImpl,
 ) {
-   suspend operator fun invoke() = networkRepository
-      .getAllHits()
-      ?.map { hit ->
-         hit.networkToDomain()
-      }
+    suspend operator fun invoke() = networkRepository
+        .getAllHits()
+        ?.map { hit ->
+            hit.networkToDomain()
+        }
 }
