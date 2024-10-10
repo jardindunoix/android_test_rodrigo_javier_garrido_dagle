f920810d5c21bd1f8983792eb13bb100ca51d482
rj <rj@gigioberto.com>
2024-10-10 19:08:53 -0300

    fixed the issue swiping

--- app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/ui/screens/MainListScreen.kt
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/ui/screens/MainListScreen.kt
@@ -2,18 +2,15 @@
 
 package cl.rodrigo_javier_garrido_dagle.mobiledevtest.ui.screens
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.MainViewModel
 import cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.model.Hit
@@ -22,6 +19,7 @@ import cl.rodrigo_javier_garrido_dagle.mobiledevtest.ui.screens.components.Swipe
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.google.gson.Gson
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,79 +46,22 @@ fun MainListScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
-            contentPadding = PaddingValues(0.dp),
+//            contentPadding = PaddingValues(0.dp),
         ) {
-
-
-            val itemCount = hits.size
             items(
-//                itemCount
                 items = hits,
-                key = { it }
-            ) { item ->
-                val dismissState = rememberSwipeToDismissBoxState(
-                    confirmValueChange = { boxToSwipe ->
-                        true
-
-
-//                        if (boxToSwipe == SwipeToDismissBoxValue.EndToStart) {
-//                            Log.d(TAG, "MainListScreen: $item")
-//                            var itemToDelete: Hit? = null
-//
-//                            for (innerItem in hits) {
-//                                if (hits[item].createdAtI == innerItem.createdAtI) {
-//                                    itemToDelete = hits[item]
-//                                    break
-//                                }
-//                            }
-//
-//                            Log.d(TAG, "${itemToDelete?.createdAtI}")
-//                        hits.remove(itemToDelete)
-//                            true
-//                        } else {
-//                            false
-//                        }
-
-
-                    }
-                )
+                key = { Gson().toJson(it) }
+            ) { hit ->
 
                 SwipeToDeleteContainer(
-                    item = language,
+                    item = hit,
                     onDelete = {
-                        programmingLanguages -= language
+                        hits -= hit
                     }
-                    /*
-                       state = dismissState,
-                       backgroundContent = {
-                           Row(
-                               modifier = Modifier
-                                   .padding(vertical = 10.dp),
-
-                               ) {
-                               Box(
-                                   modifier = Modifier
-   //                                    .fillMaxHeight()
-   //                                    .fillMaxWidth()
-                                       .background(Color.Red)
-                                       .padding(horizontal = 120.dp, vertical = 80.dp)
-                                       .align(Alignment.CenterVertically),
-                                   contentAlignment = Alignment.CenterEnd,
-                               ) {
-                                   Text(
-                                       text = "Delete",
-                                       color = Color.White,
-                                       fontWeight = FontWeight.Bold,
-                                   )
-                               }
-                           }
-                       }
-   */
-
-                ) {
+                ) { innerHit ->
                     ColumnItem(
                         modifier,
-                        hit = hits[item],
+                        hit = innerHit,
                         navController = navController,
                     )
                 }
--- app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/ui/screens/components/SwipeToDeleteComponents.kt
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/ui/screens/components/SwipeToDeleteComponents.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxState
 import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
@@ -66,16 +67,15 @@ fun <T> SwipeToDeleteContainer(
             shrinkTowards = Alignment.Top
         ) + fadeOut()
     ) {
-//        SwipeToDismissBox(
-//            state = state,
-//            backgroundContent = {
-//                DeleteBackground(swipeDismissState = state)
-//
-//
-//            }
+        SwipeToDismissBox(
+            content = { content(item) },
+            state = state,
+            backgroundContent = {
+                DeleteBackground(swipeDismissState = state)
+            }
 //            dismissContent = { content(item) },
 //            directions = setOf(DismissDirection.EndToStart)
-//        )
+        )
     }
 }
 
